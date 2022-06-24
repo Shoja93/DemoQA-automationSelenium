34176737282616eac00dbfe6c0e0267b9b01696e
shoja93 <shoja93ue@gmail.com>
2022-06-24 14:31:27 +0200

    driver

--- src/test/java/demoQA/test/bookstoreapptest/ProfileTest.java
+++ src/test/java/demoQA/test/bookstoreapptest/ProfileTest.java
@@ -1,4 +1,12 @@
 package demoQA.test.bookstoreapptest;
 
+import org.openqa.selenium.WebDriver;
+
 public class ProfileTest {
+
+    public WebDriver driver;
+
+    public ProfileTest(WebDriver driver) {
+        this.driver = driver;
+    }
 }
